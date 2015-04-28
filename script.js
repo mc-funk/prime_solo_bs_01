d7eaedb854f510e691b7113c0c90df8384456793
Michelle C. Funk <michelle@michellefunk.com>
2015-04-28 15:18:03 -0500

    Working, with remove adjust ... for the first one, anyway.

--- script.js
+++ script.js
@@ -1,34 +1,64 @@
 var apikey = '8e59aef0a8bcad6ac1ccbb6c83bd9533ff6d71d5'; // Put your API key here
 var i;
+var j;
 var rowCount = 0;
+var searchTerm;
+var adjust = 0;
+var removed;
 
 // Use this function to do stuff with your results. 
 // It is called after 'search' is executed.
 function searchCallback(results) {
+    console.log($(".searchResults .removed"));
+    $(".searchResults .removed").each(function () {
+        removed = $(this).data("index");
+        console.log("removed in callback for children: " + removed);
+    });
+    console.log("result of child search for removed: ", $(".searchResults").children(".removed"));
+    console.log("value of removed is: " + removed);
+    $('.searchResults').empty();
+    rowCount = 0;
     console.log(results);
     for (i = 0; i < 9; i++) {
-        if (i % 3 == 0) { //check to see if game should be start of a new row, based on i
-            rowCount++;
-            $(".searchResults").append("<div class='row' id='row" + rowCount + "'></div>");
+        console.log("removed: " + removed);
+        console.log(i + "is equal to removed:" + (i == removed));
+        if (i != removed) {
+            j = i - adjust;
+            if (j % 3 == 0) { //check to see if game should be start of a new row, based on j
+                rowCount++;
+                $(".searchResults").append("<div class='row' id='row" + rowCount + "'></div>");
+            }
+            $("#row" + rowCount).append(
+                "<div data-index=" + i + " class='result col-md-4 well' id='result" + i + "'></div>");
+            $("#result" + i).append(
+                "<div class='hidden-sm hidden-xs'><img src='" + results[i].image.small_url + "' ></div>");
+            $("#result" + i).append(
+                "<p class='lead gameTitle'>" + results[i].name + "</p>");
+            $("#result" + i).append(
+                "<p>Release Date: " + results[i].original_release_date + "</p>");
+            $("#result" + i).append(
+                "<p>" + results[i].deck + "</p>");
+            $("#result" + i).append(
+                "<div class='btn btn-default btn-sm removeBtn'>Remove Title</div>");
+        } else {
+            adjust += 1;
+            console.log("adjust set +1");
         }
-        $("#row" + rowCount).append("<div class='result col-md-4 well' id='result" + i + "'></div>");
-        $("#result" + i).append(
-            "<p><img src='" + results[i].image.small_url + "' ></p>"
-        );
-        $("#result" + i).append(
-            "<p class='gameTitle'>"+ results[i].name + "</p>");
-        $("#result" + i).append(
-            "<p>Release Date: "+ results[i].original_release_date + "</p>");
-        $("#result" + i).append(
-            "<p>"+ results[i].deck + "</p>");
     }
-
+    adjust = 0;
+    removed = null;
 }
 
-$(document).ready(function() {
 
-    console.log(search('smash'));
-	
+
+$(document).ready(function() {
+    searchTerm = 'smash';
+    console.log(search(searchTerm));
+	$('.searchResults').on("click", ".removeBtn", function () {
+        $(this).parent(".result").fadeOut(300).addClass("removed");
+        console.log("data(index) of removed: " + $(this).parent(".result").data("index"));
+        search(searchTerm);
+    })
 });
 
 // HELPER FUNCTION
@@ -53,8 +83,7 @@ function search(query){
 
 /*
 Start with search in JavaScript*/
-//TODO: Each title should be in a well that has an overridden color in your own stylesheet.
-//TODO: The image should only display on LG and MD displays and should hide for SM and XS displays.
-//TODO: The title should be in a p tag and have a lead class.
-//TODO:  Finally, include a button that has the bootstrap styling to make it small with the success styling.
-// This button should remove the entire game title from the page (the content need not update in the html page to adjust rows)
+//DONE: Each title should be in a well that has an overridden color in your own stylesheet.
+//DONE: The image should only display on LG and MD displays and should hide for SM and XS displays.
+//DONE: The title should be in a p tag and have a lead class.
+//DONE:  Finally, include a button that has the bootstrap styling to make it small with the success styling.This button should remove the entire game title from the page (the content need not update in the html page to adjust rows)
