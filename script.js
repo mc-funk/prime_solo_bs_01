1c6f4af7093294c92bff58637b81d0e8538c33f8
Michelle C. Funk <michelle@michellefunk.com>
2015-04-28 13:17:46 -0500

    Initial scripts and logic

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
-var apikey = ''; // Put your API key here
-
+var apikey = '8e59aef0a8bcad6ac1ccbb6c83bd9533ff6d71d5'; // Put your API key here
+var i;
+var rowCount = 0;
 
 // Use this function to do stuff with your results. 
 // It is called after 'search' is executed.
 function searchCallback(results) {
     console.log(results);
+    for (i = 0; i < 9; i++) {
+        if (i % 3 == 0) { //check to see if game should be start of a new row, based on i
+            rowCount++;
+            $(".searchResults").append("<div class='row' id='row" + rowCount + "'></div>");
+        }
+        $("#row" + rowCount).append("<div class='result col-md-4 well' id='result" + i + "'></div>");
+        $("#result" + i).append(
+            "<p><img src='" + results[i].image.small_url + "' ></p>"
+        );
+        $("#result" + i).append(
+            "<p class='gameTitle'>"+ results[i].name + "</p>");
+        $("#result" + i).append(
+            "<p>Release Date: "+ results[i].original_release_date + "</p>");
+        $("#result" + i).append(
+            "<p>"+ results[i].deck + "</p>");
+    }
+
 }
 
 $(document).ready(function() {
 
-	// Start the search here!
+    console.log(search('smash'));
 	
 });
 
@@ -32,3 +50,11 @@ function search(query){
 	});
 
 }
+
+/*
+Start with search in JavaScript*/
+//TODO: Each title should be in a well that has an overridden color in your own stylesheet.
+//TODO: The image should only display on LG and MD displays and should hide for SM and XS displays.
+//TODO: The title should be in a p tag and have a lead class.
+//TODO:  Finally, include a button that has the bootstrap styling to make it small with the success styling.
+// This button should remove the entire game title from the page (the content need not update in the html page to adjust rows)
